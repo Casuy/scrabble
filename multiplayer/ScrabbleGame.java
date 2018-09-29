7c17b480fa1160ee865c6811a149dab4d7d96521
Kuan Tian <sky950806@hotmail.com>
2018-09-29 23:06:45 +1000

    multiplayer ver1

---
+++ multiplayer/ScrabbleGame.java
@@ -0,0 +1,171 @@
+
+/**
+  * 2018-09-18
+  */
+
+
+import java.util.ArrayList;
+
+public class ScrabbleGame {
+	private char[][] scrabbleGrid;
+	private ScrabblePlayer[] playerList;
+	private int playerNumber;
+	private int passPlayers;
+	private int current;
+	private int votenumber = 0;
+
+	// 接受ScrabblePlayer类的ArrayList作为参数，初始化游戏
+	public ScrabbleGame(ArrayList<ScrabblePlayer> pList) {
+		scrabbleGrid = new char[20][20];
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 20; j++) {
+				scrabbleGrid[i][j] = ' ';
+			}
+		}
+		playerNumber = pList.size();
+		playerList = new ScrabblePlayer[playerNumber];
+		for (int i = 0; i < playerNumber; i++) {
+			playerList[i] = pList.get(i);
+		}
+		current = 0;
+		passPlayers = 0;
+	}
+
+	// 根据坐标获得一个String，坐标中的横纵坐标至少有一组是相同的（由游戏过程保证）
+	public String word(int x1, int y1, int x2, int y2) {
+		int wordLength = Math.max(x2 - x1 + 1, y2 - y1 + 1);
+		String word = null;
+		if(x1==x2 && y1==y2) {
+			word = String.valueOf(scrabbleGrid[x1][y1]);
+			return word;
+		}
+		char[] wordChar = new char[wordLength];
+		if (x1 - x2 == 0) {
+			for (int i = y1; i <= y2; i++) {
+				wordChar[i] = scrabbleGrid[x1][i];
+			}
+		} else {
+			for (int i = x1; i <= x2; i++) {
+				wordChar[i] = scrabbleGrid[i][y1];
+			}
+		}
+		word = (new String(wordChar)).trim();
+		return word;
+	}
+
+	// 广播消息
+	public void broadcast(String operation) {
+		for (int i = 0; i < playerNumber; i++) {
+			playerList[i].SetOperation(operation);
+		}
+	}
+
+	/**
+	 * 让当前玩家外的其他玩家对一个词投票，一致通过则返回true，出错视为不通过 public boolean vote(int current, String
+	 * word) { int voteNumber = 1; for (int i = (current + 1) % playerNumber; i !=
+	 * current; i = (i + 1) % playerNumber) { boolean voted = false; try { voted =
+	 * playerList[i].vote(playerList[current].getUserName(), word); } catch
+	 * (Exception e) {
+	 * 
+	 * } if (voted) voteNumber++; } if (voteNumber == playerNumber) { return true; }
+	 * return false; }
+	 **/
+
+	// 使某个玩家得分
+	public void gainScore(int player, String word) {
+		playerList[player].setScore(word.length() + playerList[player].getScore());
+	}
+
+	public void play() {
+		broadcast("Game Start");
+		String voteWord = null;
+		while (passPlayers < playerNumber) {
+			// 新一轮，重计弃权玩家
+			if (current == 0) {
+				passPlayers = 0;
+			}
+			String operation = null;
+			// 从GUI中读取玩家的操作，以String传递给服务器，如果出错视为pass
+			try {
+				while (true) {
+					operation = playerList[current].getOperation();
+					if (operation != null)
+						break;
+				}
+			} catch (Exception e) {
+
+			}
+			System.out.println(operation);
+			String[] operations = operation.split("::@:");
+			// 操作的String设计为只有两种形式，"PASS"或者是坐标+字符（以\t分割），如"E 8 A"
+			if (operations[0].equals("Pass")) {
+				passPlayers += 1;
+			} else if (operations[0].equals("Set")) {
+				String[] setword = operations[1].split(" ");
+				int x = Integer.parseInt(setword[1]) - 1;
+				int y = setword[0].toUpperCase().toCharArray()[0] - 'A';
+				char a = setword[2].toUpperCase().toCharArray()[0];
+				scrabbleGrid[x][y] = a;
+				for(int i=0;i<20;i++) {
+					for(int j=0;j<20;j++) {
+						System.out.println(scrabbleGrid[i][j]+" ");
+					}
+					System.out.println();
+				}
+				broadcast(operation);
+			} else if (operations[0].equals("Vote")) {
+				String voteMessage = operations[1];
+				String[] message = voteMessage.split(" ");
+				int y1 = message[0].toUpperCase().toCharArray()[0] - 'A';
+				int y2 = message[1].toUpperCase().toCharArray()[0] - 'A';
+				int x1 = Integer.parseInt(message[2]) - 1;
+				int x2 = Integer.parseInt(message[3]) - 1;
+				voteWord = word(x1, y1, x2, y2);
+				String m = "Vote::@:" + playerList[current].getUserName() + "::@:" + voteWord;
+				broadcast(m);
+
+			} else if (operations[0].equals("VoteYes")) {
+				votenumber += 1;
+				if (votenumber == playerNumber) gainScore(current, voteWord);
+				broadcast("The vote now is"+votenumber);
+			}
+
+			/**
+			 * 玩家执行操作之后询问是否投票，出错视作放弃投票 boolean vote = false; try { vote =
+			 * playerList[current].getVote(); } catch (Exception e) {
+			 * 
+			 * } if (vote) {
+			 * 
+			 * // 投票信息，设计为只有两种形式 // 以"\t"切分后长度为3（类似"X E G"，"Y 5 8"），代表投票一个词（与新加字符同行或同列） //
+			 * 或6 （类似"X E G Y 5 8"），代表同时投票两个词（其一与新加字符同行，其一同列） String voteMessage =
+			 * playerList[current].getVoteMessage(); String[] message =
+			 * voteMessage.split("\t"); if (message.length > 3) { int y1 =
+			 * message[1].toUpperCase().toCharArray()[0] - 'A'; int y2 =
+			 * message[2].toUpperCase().toCharArray()[0] - 'A'; String voteWord = word(x, x,
+			 * y1, y2); boolean allVote = vote(current, voteWord); if (allVote) {
+			 * gainScore(current, voteWord); } int x1 = Integer.parseInt(message[4]) - 1;
+			 * int x2 = Integer.parseInt(message[5]) - 1; voteWord = word(x1, x2, y, y);
+			 * allVote = vote(current, voteWord); if (allVote) { gainScore(current,
+			 * voteWord); } } else { if (message[0].equals("X")) { int x1 =
+			 * message[1].toUpperCase().toCharArray()[0] - 'A'; int x2 =
+			 * message[2].toUpperCase().toCharArray()[0] - 'A'; String voteWord = word(x1,
+			 * x2, y, y); boolean allVote = vote(current, voteWord); if (allVote) {
+			 * gainScore(current, voteWord); } } else { int y1 =
+			 * Integer.parseInt(message[1]) - 1; int y2 = Integer.parseInt(message[2]) - 1;
+			 * String voteWord = word(x, x, y1, y2); boolean allVote = vote(current,
+			 * voteWord); if (allVote) { gainScore(current, voteWord); } } } } }
+			 **/
+			current = current < playerNumber - 1 ? current + 1 : 0;
+		}
+		int maxscore = 0;
+		String winner = "";
+		for (int i = 0; i < playerNumber; i++) {
+			if (playerList[i].getScore() > maxscore) {
+				maxscore = playerList[i].getScore();
+				winner = playerList[i].getUserName();
+			}
+		}
+		broadcast("GameOver::@:" + winner + "::@:" + maxscore);
+
+	}
+}
---
+++ multiplayer/ScrabblePanel.java
@@ -0,0 +1,444 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+
+
+
+/**
+  * 2018-09-17
+  */
+
+public class ScrabblePanel {
+	private JFrame frmScrabble;
+	private JPanel gameBoard;
+	private JPanel menu;
+	private static JTextField[][] board;
+	private JButton btnInput;
+	private JButton btnVoteWord;
+	private JButton btnVote;
+	private static JTextArea MessageBoard;
+	
+	private JButton btnPlay;
+	private ScrabbleClient sc;
+	private JButton btnPass;
+	private JPanel alp;
+	private JPanel dig;
+	
+	/**
+	 * Create the application.
+	 */
+	public ScrabblePanel() {
+		sc = new ScrabbleClient();
+		initialize();
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					new ScrabblePanel();	
+				} catch (Exception e) {
+					e.printStackTrace();
+					System.exit(0);
+				}
+			}
+		});
+	}
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	@SuppressWarnings("deprecation")
+	private void initialize() {
+		frmScrabble = new JFrame();
+		frmScrabble.setTitle("Scrabble");
+		frmScrabble.setBounds(100, 100, 810, 611);
+		frmScrabble.getContentPane().setLayout(null);
+
+		// 20*20 gameboard
+		gameBoard = new JPanel(new GridLayout(20, 20));
+		gameBoard.setBounds(33, 34, 500, 500);
+		frmScrabble.getContentPane().add(gameBoard);
+			
+		// control pane
+		menu = new JPanel();
+		menu.setBounds(561, 34, 217, 500);
+		frmScrabble.getContentPane().add(menu);
+		menu.setLayout(null);
+		
+		btnInput = new JButton("Input");
+		btnInput.setBounds(55, 26, 125, 31);
+		btnInput.setContentAreaFilled(false);
+		btnInput.setFont(new Font("Calibri", Font.PLAIN, 18));
+		menu.add(btnInput);
+		
+		btnPlay = new JButton("Play");
+		btnPlay.setBounds(55, 202, 125, 31);
+		btnPlay.setContentAreaFilled(false);
+		btnPlay.setFont(new Font("Calibri", Font.PLAIN, 18));
+		menu.add(btnPlay);
+		
+		btnVoteWord = new JButton("VoteWord");
+		btnVoteWord.setBounds(55, 70, 125, 31);
+		btnVoteWord.setContentAreaFilled(false);
+		btnVoteWord.setFont(new Font("Calibri", Font.PLAIN, 18));
+		menu.add(btnVoteWord);
+		
+		
+		btnVote = new JButton("Vote");
+		btnVote.setBounds(55, 114, 125, 31);
+		btnVote.setContentAreaFilled(false);
+		btnVote.setFont(new Font("Calibri", Font.PLAIN, 18));
+		menu.add(btnVote);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 256, 217, 244);
+		menu.add(panel);
+		panel.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 217, 244);
+		panel.add(scrollPane);
+		
+		MessageBoard = new JTextArea();
+		MessageBoard.setLineWrap(true);
+		scrollPane.setViewportView(MessageBoard);
+		
+		btnPass = new JButton("Pass");
+		btnPass.setBounds(55, 158, 125, 31);
+		btnPass.setContentAreaFilled(false);
+		btnPass.setFont(new Font("Calibri", Font.PLAIN, 18));
+		menu.add(btnPass);
+		
+		//20*1 and 1*20 label
+		alp = new JPanel();
+		alp.setBounds(33, 9, 500, 25);
+		frmScrabble.getContentPane().add(alp);
+		alp.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		dig = new JPanel();
+		dig.setBounds(9, 34, 25, 500);
+		frmScrabble.getContentPane().add(dig);
+		dig.setLayout(new GridLayout(0, 1, 0, 0));
+
+		// create 19*19 textfields in the gameboard
+		board = new JTextField[20][20];
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 20; j++) {
+				board[i][j] = new JTextField();
+				board[i][j].setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
+
+				if ((i == j || i + j == 19) && (i < 6 || i == 8 || i > 10)) {
+					board[i][j].setBackground(Color.RED);
+				}
+
+				else if (((i == 3 || i == 9 || i == 15) && (j == 0 || j == 19))
+						|| ((j == 3 || j == 9 || j == 15) && (i == 0 || i == 19))) {
+					board[i][j].setBackground(Color.BLUE);
+				}
+
+				else if (((i - j) == 5 && (j > 8 || j < 4) && j != 0 && i != 13 && i != 19)) {
+					board[i][j].setBackground(Color.GREEN);
+					board[j][i].setBackground(Color.GREEN);
+				}
+
+				else if ((i + j) == 13 && (i > 8 || i < 5) && i != 0 && j != 0) {
+					board[i][j].setBackground(Color.GREEN);
+				}
+
+				else if ((i + j) == 23 && (j < 10 || j > 14) && i != 19 && j != 19) {
+					board[i][j].setBackground(Color.GREEN);
+				}
+
+				else {
+					board[i][j].setHorizontalAlignment(JTextField.CENTER);
+					board[i][j].setFont(new Font("Arial", Font.PLAIN, 16));
+					board[i][j].setBackground(Color.WHITE);
+					board[i][j].setDocument(new LimitDoc());
+
+				}
+				gameBoard.add(board[i][j]);
+			}
+		}
+		
+		//labels shows the grid
+		JTextField[] alpha = new JTextField[20];
+		JTextField[] digit = new JTextField[20];
+		char alpha_ = 'A';
+		int digit_ = 1;
+		for(int i=0; i<20;i++) {
+			alpha[i] = new JTextField();
+			alpha[i].setText(String.valueOf(alpha_));
+			alpha[i].setHorizontalAlignment(JTextField.CENTER);
+			alpha[i].setFont(new Font("Arial", Font.BOLD, 16));
+			alpha[i].disable();
+			alp.add(alpha[i]);
+			digit[i] = new JTextField();
+			digit[i].setText(String.valueOf(digit_));
+			digit[i].setHorizontalAlignment(JTextField.CENTER);
+			digit[i].setFont(new Font("Arial", Font.BOLD, 16));
+			digit[i].disable();
+			dig.add(digit[i]);
+			alpha_++;
+			digit_++;	
+		}
+		
+		frmScrabble.setVisible(true);
+		event();
+	}
+		
+	//加字母到特定位置
+	public static void setWord(String operation) {
+		String[] operations = operation.split(" ");
+		int x = Integer.parseInt(operations[1]) - 1;
+		int y = operations[0].toUpperCase().toCharArray()[0] - 'A';
+		char a = operations[2].toUpperCase().toCharArray()[0];
+		board[x][y].setText(String.valueOf(a));
+	}
+	
+	//将各类message展示在消息板上
+	public static void showMessage(String message) {
+		String[] messages = message.split("::@:");
+		if(messages[0].equals("Set")) {
+			setWord(messages[1]);
+		}
+		if(messages[0].equals("Vote")) {
+			MessageBoard.append(messages[1]+" is voting for the word "+messages[2]+"\r\n");
+		}
+		else if(messages[0].equals("GameOver")) {
+			MessageBoard.append("GameOver, the winner is "+messages[1]+" with a score of "+messages[2]+"\r\n");
+		}
+		else {
+			MessageBoard.append(message+"\r\n");
+		}
+	}
+
+	//各个button的点击事件
+	private void event(){
+		//input button弹出窗口输入字符
+		btnInput.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				new InputField();
+			}
+		});	
+		btnVoteWord.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				new Vote();
+			}
+		});	
+		btnPlay.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				sc.operate("Play::@:");
+				
+			}
+		});	
+		btnPass.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				sc.operate("Pass::@:");
+				
+			}
+		});
+		btnVote.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				sc.operate("VoteYes::@:");
+				
+			}
+		});
+	}
+	
+	
+	//添加字母的窗口
+	class InputField {
+
+		private JFrame frame;
+		private JTextField row;
+		private JTextField letter;
+		private JTextField col;
+
+		/**
+		 * Create the application.
+		 */
+		public InputField() {
+			initialize();
+		}
+
+		/**
+		 * Initialize the contents of the frame.
+		 */
+		private void initialize() {
+			frame = new JFrame();
+			frame.setBounds(100, 100, 269, 269);
+			frame.getContentPane().setLayout(null);
+			
+			JLabel lblCol = new JLabel("Col");
+			lblCol.setBounds(28, 51, 72, 18);
+			frame.getContentPane().add(lblCol);
+			
+			JLabel lblRow = new JLabel("Row");
+			lblRow.setBounds(103, 51, 72, 18);
+			frame.getContentPane().add(lblRow);
+			
+			row = new JTextField();
+			row.setBounds(103, 82, 38, 24);
+			row.setDocument(new LimitDoc());
+			frame.getContentPane().add(row);
+			row.setColumns(10);
+			
+			JLabel lblLetter = new JLabel("Letter");
+			lblLetter.setBounds(167, 51, 72, 18);
+			frame.getContentPane().add(lblLetter);
+			
+			letter = new JTextField();
+			letter.setBounds(167, 81, 49, 24);
+			frame.getContentPane().add(letter);
+			letter.setColumns(10);
+			letter.setDocument(new LimitDoc());
+			
+			JButton btnInput = new JButton("Confirm");
+			btnInput.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					String operation = "Set::@:"+col.getText()+" "+row.getText()+" "+letter.getText();
+					sc.operate(operation);					
+					frame.dispose();
+				}
+			});
+			btnInput.setBounds(65, 159, 113, 27);
+			frame.getContentPane().add(btnInput);
+			
+			col = new JTextField();
+			col.setBounds(28, 82, 38, 24);
+			frame.getContentPane().add(col);
+			col.setColumns(10);
+			col.setDocument(new LimitDoc());
+			frame.setVisible(true);
+		}		
+	}
+	
+	
+	class Vote {
+
+		private JFrame frame;
+		private JTextField text_x1;
+		private JTextField text_y1;
+		private JTextField text_x2;
+		private JTextField text_y2;
+
+		/**
+		 * Create the application.
+		 */
+		public Vote() {
+			initialize();
+		}
+
+		/**
+		 * Initialize the contents of the frame.
+		 */
+		private void initialize() {
+			frame = new JFrame();
+			frame.setBounds(100, 100, 450, 300);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.getContentPane().setLayout(null);
+			
+			JLabel lblX = new JLabel("x1:");
+			lblX.setBounds(39, 43, 72, 18);
+			frame.getContentPane().add(lblX);
+			
+			JLabel lblY = new JLabel("y1:");
+			lblY.setBounds(196, 43, 72, 18);
+			frame.getContentPane().add(lblY);
+			
+			JLabel lblX_1 = new JLabel("x2:");
+			lblX_1.setBounds(39, 106, 72, 18);
+			frame.getContentPane().add(lblX_1);
+			
+			JLabel lblY_1 = new JLabel("y2:");
+			lblY_1.setBounds(196, 106, 72, 18);
+			frame.getContentPane().add(lblY_1);
+			
+			text_x1 = new JTextField();
+			text_x1.setBounds(72, 40, 86, 24);
+			frame.getContentPane().add(text_x1);
+			text_x1.setColumns(10);
+			
+			text_y1 = new JTextField();
+			text_y1.setBounds(237, 40, 86, 24);
+			frame.getContentPane().add(text_y1);
+			text_y1.setColumns(10);
+			
+			text_x2 = new JTextField();
+			text_x2.setBounds(72, 103, 86, 24);
+			frame.getContentPane().add(text_x2);
+			text_x2.setColumns(10);
+			
+			text_y2 = new JTextField();
+			text_y2.setBounds(237, 106, 86, 24);
+			frame.getContentPane().add(text_y2);
+			text_y2.setColumns(10);
+			
+			text_x1.setDocument(new LimitDoc());
+			text_x2.setDocument(new LimitDoc());
+			text_y1.setDocument(new LimitDoc());
+			text_y2.setDocument(new LimitDoc());
+			
+			JButton btnConfirm = new JButton("Confirm");
+			btnConfirm.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					String operation = "Vote::@:"+text_x1.getText()+" "+text_x2.getText()+" "+text_y1.getText()+" "+text_y2.getText();
+					sc.operate(operation);
+					
+					frame.dispose();
+				}
+			});
+			btnConfirm.setBounds(72, 182, 113, 27);
+			frame.getContentPane().add(btnConfirm);
+			frame.setVisible(true);
+		}
+	}
+	
+	
+}
+
+
+
+
+// limit the length of the document 1
+@SuppressWarnings("serial")
+class LimitDoc extends PlainDocument {
+	private final int maxLength = 1;
+
+	public void insertString(int offset, String str, AttributeSet a) throws BadLocationException {
+		if (getLength() + str.length() > maxLength) {
+			return;
+		} else {
+			super.insertString(offset, str, a);
+		}
+	}
+}
+
+
